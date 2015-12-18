6d9c713a15c07bcd32ecbe6d2e50c6e69db3a784
Ethan <Ethan_le95@hotmail.com>
2015-12-17 23:21:27 -0500

    Added Camera rotation
    Rotates when it hits a certain trigger
    Added trigger boxes
    Added a prototype human player

--- PartyPoopers/Assets/Scripts/CameraControls.cs
+++ PartyPoopers/Assets/Scripts/CameraControls.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControls : MonoBehaviour {
+public class CameraControls : MonoBehaviour
+{
+    public float m_AngleRotation = 90;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    GameObject m_Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider aCollider)
+    {
+        if (aCollider.tag == "Player")
+        {
+            //Ease the rotation.
+            Easing.StartTween(this, Easing.Type.BounceIn, 0, 1, 0.3f, rotate);
+            Debug.Log("I got hit");
+        }
+    }
+
+    private void rotate(float aRotate)
+    {
+        //Checks if the angle is at a certain degree rotate it properly.
+        if (m_AngleRotation == 0 && m_Player.transform.eulerAngles.y >= 260)
+        {
+            //Lerp the camera for a smooth follow.
+            m_Player.transform.eulerAngles = Vector3.Lerp(new Vector3(0, -90, 0), new Vector3(0, m_AngleRotation, 0), aRotate);
+        }
+        else if (m_AngleRotation == 270 && m_Player.transform.eulerAngles.y <= 1)
+        {
+            m_Player.transform.eulerAngles = Vector3.Lerp(new Vector3(0, 360, 0), new Vector3(0, m_AngleRotation, 0), aRotate);
+        }
+        else
+        {
+            m_Player.transform.eulerAngles = Vector3.Lerp(m_Player.transform.eulerAngles, new Vector3(0, m_AngleRotation, 0), aRotate);
+        }
+
+    }
 }
--- PartyPoopers/Assets/Scripts/Player.cs
+++ PartyPoopers/Assets/Scripts/Player.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerClass : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    //This class is a testing prototype. It will be removed later.
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Vector3 movement;
+    float horizontal;
+    float vertical;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(horizontal, 0.0f, vertical);
+
+        transform.Translate(movement * Time.deltaTime * 5);
+    }
 }
